0532df201465b082b146f65a49f41a9d4a4f58d1
Colin MacKenzie IV <sinisterchipmunk@gmail.com>
2011-11-28 02:10:55 -0500

    3.1 - Jax on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dungeon::Application.config.secret_token = 'bdea70ae13e34e93a25bcf0da014720335337a4020a513e5328d808151a6dbc9a976abdd8bb630db9d1b9f6c15c434d72d1147063d5838ee65545764126114d5'
